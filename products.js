5f6811cf8dbdaa7e8ca501d8ce22a66b87cdd5cf
Emil Joseph <emiljoseph89@gmail.com>
2020-11-18 13:00:36 -0700

    gallery objects - should update the keys and values to match our gallery

---
+++ products.js
@@ -0,0 +1,61 @@
+
+const products = [
+  {
+    id: 1,
+    title: 'creative-water',
+    imagePath: 'images/01-creative-waters.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of cbd drink'
+  },
+  {
+    id: 2,
+    title: 'camino-candy',
+    imagePath: 'images/02-camino-candy.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of cbd infused candy'
+  },
+  {
+    id: 3,
+    title: 'cbd-oil',
+    imagePath: 'images/03-cbd-oil.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of a cbd oil product'
+  },
+  {
+    id: 4,
+    title: 'cann',
+    imagePath: 'images/04-cann.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of cannabis infused candy'
+  },
+  {
+    id: 5,
+    title: 'nux',
+    imagePath: 'images/05-nux.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of a cbd soft get product'
+  },
+  {
+    id: 6,
+    title: 'terra',
+    imagePath: 'images/06-terra.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of cbd product'
+  },
+  {
+    id: 7,
+    title: 'cbd-coffee',
+    imagePath: 'images/07-cbd-coffee.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of a cbd infused coffee product'
+  },
+  {
+    id: 8,
+    title: 'petra',
+    imagePath: 'images/08-petra.jpg',
+    credit: 'https://unsplash.com/s/photos/thc-cbd-products',
+    description: 'picture of cannabis infused mints'
+  }
+];
+
+module.exports = products;
